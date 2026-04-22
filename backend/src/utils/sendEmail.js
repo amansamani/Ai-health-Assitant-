efa16902c60256892b7215e3ced369e200b1a24f
aman <amanworkinfo@gmail.com>
2026-04-22 12:16:03 +0530

    adding the dns to resned from vercel

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (to, otp) => {
   const resend = new Resend(process.env.RESEND_API_KEY); // ✅ inside function
 
   await resend.emails.send({
-    from: "FitLip <onboarding@resend.dev>",
+    from:"FitLip <noreply@amansamani.me>",
     to,
     subject: "Your OTP Code",
     html: `<h2>Your OTP is: <strong>${otp}</strong></h2><p>Expires in 10 minutes.</p>`,
